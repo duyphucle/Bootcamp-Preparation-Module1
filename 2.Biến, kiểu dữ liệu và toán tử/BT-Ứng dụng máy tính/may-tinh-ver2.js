bf38b04a312597d747e84310e474c1c25dd25e94
duy phuc <malangcasino@gmail.com>
2019-09-12 21:19:30 +0700

    May tinh ver2

--- 2.Biến, kiểu dữ liệu và toán tử/BT-Ứng dụng máy tính/may-tinh-ver2.js
+++ 2.Biến, kiểu dữ liệu và toán tử/BT-Ứng dụng máy tính/may-tinh-ver2.js
@@ -1,6 +1,7 @@
-function Pro(a, b, c) {
+function proMath(a, b, c) {
     if (c == 1) {
-        let x1 = parseInt(a);
+        let x1, x2, x3;
+        x1 = parseInt(a);
         x2 = parseInt(b);
         x3 = x1 + x2;
         document.getElementById("demo").innerHTML = "Result Division:" + x3;
